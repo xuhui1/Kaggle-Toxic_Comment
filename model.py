b12e242585096d27e8f779a34385935dbf13ffbf
xuhui1 <731048736@qq.com>
2018-03-18 15:43:11 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,25 +1,64 @@
 
 # coding = utf-8
-from keras.layers import Dense, Input, LSTM, Bidirectional, Conv1D
-from keras.layers import Dropout, Embedding
+from keras.layers import Dense, Input, LSTM, Bidirectional, Conv1D, Conv2D
+from keras.layers import Dropout, Embedding,Reshape
 
 from keras.preprocessing import text, sequence
 from keras.layers import GlobalMaxPooling1D, GlobalAveragePooling1D, concatenate, SpatialDropout1D
 from keras.models import Model
 
 def model_bilstm_cnn(sentence_len,embedding_matrix):
-  inp = Input(shape=(sentence_len,),dtype=int32)
-  x = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1], weights=[embedding_matrix], trainable=True)(inp)
-  x = SpatialDropout1D(0.35)(x)
+    inp = Input(shape=(sentence_len,),dtype=int32)
+    x = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1], weights=[embedding_matrix], trainable=True)(inp)
+    x = SpatialDropout1D(0.35)(x)
 
-  x = Bidirectional(LSTM(128, return_sequences=True, dropout=0.15, recurrent_dropout=0.15))(x)
-  x = Conv1D(64, kernel_size=3, padding='valid', kernel_initializer='glorot_uniform')(x)
+    x = Bidirectional(LSTM(128, return_sequences=True, dropout=0.15, recurrent_dropout=0.15))(x)
+    x = Conv1D(64, kernel_size=3, padding='valid', kernel_initializer='glorot_uniform')(x)
 
-  avg_pool = GlobalAveragePooling1D()(x)
-  max_pool = GlobalMaxPooling1D()(x)
-  x = concatenate([avg_pool, max_pool])
+    avg_pool = GlobalAveragePooling1D()(x)
+    max_pool = GlobalMaxPooling1D()(x)
+    x = concatenate([avg_pool, max_pool])
 
-  out = Dense(6, activation='sigmoid')(x)
+    out = Dense(6, activation='sigmoid')(x)
 
-  model = Model(inp, out)
-  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model = Model(inp, out)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+# the difference of conv1D and conv2D：http://blog.csdn.net/hahajinbu/article/details/79535172
+# conv1D: kernel_size=3 -> (3,embed_size)
+# conv2D: kernel_size=3 -> (3,3,1)  so define the kernel size=(3,embed_size) -> (3,embed_size,1)
+def model_mulit_cnn(sentence_len,embedding_matrix,num_filters=64):
+    inp = Input(shape(sentence_len,),dtype=int32)
+    x = Embedding(embedding_matrix.shape[0],embedding_matrix.shape[1],weights=[embedding_matrix],trainable=True)(inp)
+    x = SpatialDropout1D(0.4)(x)
+    x = Reshape((sentence_len,embedding_matrix.shape[1],1))(x)
+    
+    filter_sizes = [1,2,3,5]
+    
+    conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embed_size), kernel_initializer='normal',
+                                                                                    activation='elu')(x)
+    conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embed_size), kernel_initializer='normal',
+                                                                                    activation='elu')(x)
+    conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embed_size), kernel_initializer='normal',
+                                                                                    activation='elu')(x)
+    conv_3 = Conv2D(num_filters, kernel_size=(filter_sizes[3], embed_size), kernel_initializer='normal',
+                                                                                    activation='elu')(x)
+    
+    maxpool_0 = MaxPool2D(pool_size=(maxlen - filter_sizes[0] + 1, 1))(conv_0)
+    maxpool_1 = MaxPool2D(pool_size=(maxlen - filter_sizes[1] + 1, 1))(conv_1)
+    maxpool_2 = MaxPool2D(pool_size=(maxlen - filter_sizes[2] + 1, 1))(conv_2)
+    maxpool_3 = MaxPool2D(pool_size=(maxlen - filter_sizes[3] + 1, 1))(conv_3)
+        
+    z = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2, maxpool_3])   
+    z = Flatten()(z)
+    z = Dropout(0.1)(z)
+        
+    outp = Dense(6, activation="sigmoid")(z)
+    
+    model = Model(inputs=inp, outputs=outp)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+    
