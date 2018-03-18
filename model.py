8eb45e88a610c96c76221d422cea65c3764efd90
xuhui1 <731048736@qq.com>
2018-03-18 17:06:59 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 
 # coding = utf-8
-from keras.layers import Dense, Input, LSTM, Bidirectional, Conv1D, Conv2D
+from keras.layers import Dense, Input, LSTM, Bidirectional, Conv1D, Conv2D, GRU
 from keras.layers import Dropout, Embedding,Reshape
 
 from keras.preprocessing import text, sequence
@@ -35,19 +35,19 @@ def model_mulitkernel_cnn(sentence_len,embedding_matrix,num_filters=64):
     
     filter_sizes = [1,2,3,5]
     
-    conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embed_size), kernel_initializer='normal',
+    conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_matrix.shape[1]), kernel_initializer='normal',
                                                                                     activation='elu')(x)
-    conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embed_size), kernel_initializer='normal',
+    conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_matrix.shape[1]), kernel_initializer='normal',
                                                                                     activation='elu')(x)
-    conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embed_size), kernel_initializer='normal',
+    conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_matrix.shape[1]), kernel_initializer='normal',
                                                                                     activation='elu')(x)
-    conv_3 = Conv2D(num_filters, kernel_size=(filter_sizes[3], embed_size), kernel_initializer='normal',
+    conv_3 = Conv2D(num_filters, kernel_size=(filter_sizes[3], embedding_matrix.shape[1]), kernel_initializer='normal',
                                                                                     activation='elu')(x)
     
-    maxpool_0 = MaxPool2D(pool_size=(maxlen - filter_sizes[0] + 1, 1))(conv_0)
-    maxpool_1 = MaxPool2D(pool_size=(maxlen - filter_sizes[1] + 1, 1))(conv_1)
-    maxpool_2 = MaxPool2D(pool_size=(maxlen - filter_sizes[2] + 1, 1))(conv_2)
-    maxpool_3 = MaxPool2D(pool_size=(maxlen - filter_sizes[3] + 1, 1))(conv_3)
+    maxpool_0 = MaxPool2D(pool_size=(sentence_len - filter_sizes[0] + 1, 1))(conv_0)
+    maxpool_1 = MaxPool2D(pool_size=(sentence_len - filter_sizes[1] + 1, 1))(conv_1)
+    maxpool_2 = MaxPool2D(pool_size=(sentence_len - filter_sizes[2] + 1, 1))(conv_2)
+    maxpool_3 = MaxPool2D(pool_size=(sentence_len - filter_sizes[3] + 1, 1))(conv_3)
         
     z = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2, maxpool_3])   
     z = Flatten()(z)
@@ -62,3 +62,27 @@ def model_mulitkernel_cnn(sentence_len,embedding_matrix,num_filters=64):
 
     return model
 
+def model_lstm_concat_cnn(sentence_len,embedding_matrix,num_filters):
+    inp = Input(shape(sentence_len,),dtype=int32)
+    x = Embedding(embedding_matrix.shape[0],embedding_matrix.shape[1],weights=[embedding_matrix],trainable=True)(inp)
+    x = SpatialDropout1D(0.4)(x)
+    
+    cnn = Conv2D(num_filters, kernel_size=3, kernel_initializer='normal',activation='elu')(x)
+    avg_pool = GlobalAveragePooling1D()(x)
+    max_pool = GlobalMaxPooling1D()(x)
+    cnn = concatenate([avg_pool, max_pool])
+    
+    lstm =  Bidirectional(GRU(128, return_sequences=True, dropout=0.15, recurrent_dropout=0.15))(x)
+    
+    x =  Concatenate(axis=1)([cnn,lstm])
+    x = Dropout(0.4)(x)
+    outp = Dense(6, activation="sigmoid")(x)
+    
+    model = Model(inputs=inp, outputs=outp)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
+    
+    
+    
