664170e54128306f94c333475372614a6861225b
xuhui1 <731048736@qq.com>
2018-03-19 11:21:49 +0800

    Update model.py

--- model.py
+++ model.py
@@ -63,7 +63,7 @@ def model_mulitkernel_cnn(sentence_len,embedding_matrix,num_filters=64):
     return model
 
 def model_lstm_concat_cnn(sentence_len,embedding_matrix,num_filters):
-    inp = Input(shape(sentence_len,),dtype=int32)
+    inp = Input(shape=(sentence_len,),dtype='int32')
     x = Embedding(embedding_matrix.shape[0],embedding_matrix.shape[1],weights=[embedding_matrix],trainable=True)(inp)
     x = SpatialDropout1D(0.4)(x)
     
@@ -84,14 +84,14 @@ def model_lstm_concat_cnn(sentence_len,embedding_matrix,num_filters):
                   metrics=['accuracy'])
     return model
     
-def model_lstm_cnn(sentence_len,embedding_matrix):
-    inp = Input(shape(sentence_len,),dtype=int32)
+def model_cnn_lstm(sentence_len,embedding_matrix):
+    inp = Input(shape=(sentence_len,),dtype='int32')
     x = Embedding(embedding_matrix.shape[0],embedding_matrix.shape[1],weights=[embedding_matrix],trainable=True)(inp)
     x = SpatialDropout1D(0.4)(x)
     
     x = Reshape((sentence_len,embedding_matrix.shape[1],1))(x)
-    x = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_matrix.shape[1]), kernel_initializer='normal',
-                                                                                    activation='elu')(x)
+    x = Conv2D(num_filters, kernel_size=(3, embedding_matrix.shape[1]), kernel_initializer='normal',
+                                                                                    activation='relu')(x)
     x = Dropout(0.2)(x)
     x_w, x_h, x_d = [int(value) for value in x.shape[1:]]
     x = Permute((1,3,2))(x)
@@ -109,3 +109,41 @@ def model_lstm_cnn(sentence_len,embedding_matrix):
                   metrics=['accuracy'])
     return model
     
+def model_depCNN(sentence_len,embedding_matrix):
+    comment_input = Input(shape=(sentence_len,), dtype='int32')
+    embedding_layer = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1],
+                                weights=[embedding_matrix],
+                                trainable=False)
+    embedded_sequences = embedding_layer(comment_input) 
+    
+    kernel_size = 3
+    #activation_func = LeakyReLU()
+    activation_func = Activation('relu')
+
+    # Convolutional block_1
+    conv1 = Conv1D(64, kernel_size)(embedded_sequences) #64
+    act1 = activation_func(conv1)
+    bn1 = BatchNormalization()(act1)
+    pool1 = MaxPooling1D(pool_size=2, strides=2)(bn1)
+
+    conv4 = Conv1D(256, kernel_size)(pool1)
+    act4 = activation_func(conv4)
+    bn4 = BatchNormalization()(act4)
+    pool4 = MaxPooling1D(pool_size=4, strides=4)(bn4)    
+    # Global Layers
+    gmaxpl = GlobalMaxPooling1D()(pool4)
+    gmeanpl = GlobalAveragePooling1D()(pool4)
+    mergedlayer = concatenate([gmaxpl, gmeanpl], axis=1)
+    dense1 = Dense(512,
+        kernel_initializer='glorot_normal',
+        bias_initializer='glorot_normal')(mergedlayer)
+    actmlp = activation_func(dense1) 
+    reg = Dropout(0.5)(actmlp)
+    
+    dense2 = Dense(6, activation='softmax')(reg)
+    
+    model = Model(inputs=[comment_input], outputs=[dense2])
+    model.compile(loss='binary_crossentropy',
+                  optimizer=RMSprop(clipvalue=1, clipnorm=1),
+                  metrics=['accuracy'])  
+    return model
