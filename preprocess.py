80826c235530559b4d208381cf765b86e0349fba
xuhui1 <731048736@qq.com>
2018-03-19 12:36:44 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,19 +1,34 @@
 #coding=utf-8
+from keras.preprocessing import text, sequence
+import nltk
+import tqdm
 
-EMBEDDING_FILE = 'glove.300d'
-def get_coefs(word, *arr): return word, np.asarray(arr, dtype='float32')
-embeddings_index = dict(get_coefs(*o.strip().split()) for o in open(EMBEDDING_FILE))
 
-all_embs = np.stack(embeddings_index.values())
-emb_mean, emb_std = all_embs.mean(), all_embs.std()
+def getembedding_(X_train, X_test,maxlen, EMBEDDING_FILE, max_features=30000):
+    tokenizer = text.Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(list(X_train) + list(X_test))
+    X_train = tokenizer.texts_to_sequences(X_train)
+    X_test = tokenizer.texts_to_sequences(X_test)
+    x_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(X_test, maxlen=maxlen)
 
-word_index = tokenizer.word_index
-nb_words = min(max_features, len(word_index))
-embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
-for word, i in word_index.items():
-    if i >= max_features: continue
-    embedding_vector = embeddings_index.get(word)
-    if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+    def get_coefs(word, *arr):
+        return word, np.asarray(arr, dtype='float32')
+
+    embeddings_index = dict(get_coefs(*o.strip().split()) for o in open(EMBEDDING_FILE))
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean, emb_std = all_embs.mean(), all_embs.std()
+
+    word_index = tokenizer.word_index
+    nb_words = min(max_features, len(word_index))
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, len(all_embs[0])))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
+    return x_train, x_test, embedding_matrix
     
 def read_embedding_list(file_path):
     embedding_word_dict = {}
@@ -28,3 +43,31 @@ def read_embedding_list(file_path):
 
     embedding_list = np.array(embedding_list)
     return embedding_list, embedding_word_dict
+def clear_embedding_list(embedding_list, embedding_word_dict, words_dict):
+    cleared_embedding_list = []
+    cleared_embedding_word_dict = {}
+
+    for word in words_dict:
+        if word not in embedding_word_dict:
+            continue
+        word_id = embedding_word_dict[word]
+        row = embedding_list[word_id]
+        cleared_embedding_list.append(row)
+        cleared_embedding_word_dict[word] = len(cleared_embedding_word_dict)
+
+    return cleared_embedding_list, cleared_embedding_word_dict
+def tokenize_sentences(sentences, words_dict):
+    tokenized_sentences = []
+    for sentence in tqdm.tqdm(sentences):
+        if hasattr(sentence, "decode"):
+            sentence = sentence.decode("utf-8")
+        tokens = nltk.tokenize.word_tokenize(sentence)
+        result = []
+        for word in tokens:
+            word = word.lower()
+            if word not in words_dict:
+                words_dict[word] = len(words_dict)
+            word_index = words_dict[word]
+            result.append(word_index)
+        tokenized_sentences.append(result)
+    return tokenized_sentences, words_dict
